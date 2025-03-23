8d6cf824d606de717b799c6c3f0f274b745c4849
thuatdt137 <dothuat098@gmail.com>
2025-03-23 23:32:59 +0700

    Revert "Merge branch 'Vinh' into thuat"
    This reverts commit b0149cfcf64ea043e9c12e51782ca228bf1af572, reversing
    changes made to 5ab00caad75f76adb50fa0292fdb9cf7963ef344.

--- Assets/Scripts/PlayerScript/PlayerSaveHandler.cs
+++ Assets/Scripts/PlayerScript/PlayerSaveHandler.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
 using UnityEngine;
 
 public class PlayerSaveHandler : MonoBehaviour
 {
-    private SaveHouse saveHouse;
     // References to the player's components
     private PlayerMovement playerMovement;
     private ExpManager expManager;
@@ -14,8 +12,6 @@ public class PlayerSaveHandler : MonoBehaviour
     // Reference to the SaveSystem
     private SaveSystem saveSystem;
     private InventoryManager inventoryManager;
-
-    public bool canSaveLoad = false;
 
     void Awake()
     {
@@ -28,43 +24,21 @@ public class PlayerSaveHandler : MonoBehaviour
         inventoryManager = FindObjectOfType<InventoryManager>();
         // Find the SaveSystem in the scene
         saveSystem = FindObjectOfType<SaveSystem>();
-        saveHouse = FindAnyObjectByType<SaveHouse>();
     }
-    private void Start()
-    {
-        LoadPlayerData();
-    }
+
     void Update()
     {
-        if (canSaveLoad)
+        // Press S to save the game
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            // Press I to save the game
-            if (Input.GetKeyDown(KeyCode.I))
-            {
-                SavePlayerData();
-                StartCoroutine(PlaySaveAnimation());
-            }
-
-            // Press O to load the game
-            if (Input.GetKeyDown(KeyCode.O))
-            {
-                LoadPlayerData();   
-            }
+            SavePlayerData();
         }
-    }
-    private IEnumerator PlaySaveAnimation()
-    {
-        // Kích hoạt animation bằng cách đặt isPlayerInRange thành true
-        saveHouse.anim.SetBool("isPlayerInRange", true);
 
-        // Lấy thông tin về animation state hiện tại
-        float animationLength = 1f;
-
-        // Chờ cho animation chạy hết
-        yield return new WaitForSeconds(animationLength);
-
-        // Đặt lại isPlayerInRange thành false
-        saveHouse.anim.SetBool("isPlayerInRange", false);
+        // Press L to load the game
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            LoadPlayerData();
+        }
     }
 
     public void SavePlayerData()
@@ -181,38 +155,6 @@ public class PlayerSaveHandler : MonoBehaviour
         }
         else
         {
-            // Set default values if no save data exists
-            transform.position = Vector3.zero;
-            StatsManager.Instance.currentHealth = 100;
-            StatsManager.Instance.maxHealth = 100;
-            StatsManager.Instance.UpdateHealth(0);
-            expManager.currentExp = 0;
-            expManager.level = 1;
-            expManager.expToLevel = 10;
-            expManager.UpdateUI();
-            StatsManager.Instance.damage = 10;
-            StatsManager.Instance.weaponRange = 1f;
-            StatsManager.Instance.knockbackForce = 5f;
-            StatsManager.Instance.knockbackTime = 0.2f;
-            StatsManager.Instance.stunTime = 0.5f;
-            StatsManager.Instance.speed = 5;
-            StatsManager.Instance.statsUI.UpdateAllStats();
-            playerCombat.enabled = true;
-            playerBow.enabled = false;
-            changeEquipment.transform.localScale = new Vector3(1.5f, 1.5f, 0);
-
-            // Default inventory values (clear inventory)
-            foreach (var slot in inventoryManager.itemSlots)
-            {
-                slot.itemSO = null;
-                slot.quantity = 0;
-                slot.UpdateUI();
-            }
-
-            // Default gold value
-            inventoryManager.gold = 0;
-            inventoryManager.goldText.text = inventoryManager.gold.ToString();
-
             Debug.Log("No save data found. Starting with default values.");
         }
     }
